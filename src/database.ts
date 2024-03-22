86adb407462b1e125ebdcf93cd78fd3c7e776b07
diego-dps <diego.pereira2806@hotmail.com>
2024-03-22 02:22:23 -0300

    chore: configure database

---
+++ src/database.ts
@@ -0,0 +1,23 @@
+import { knex as setupKnex, Knex } from 'knex';
+import { env } from './env';
+
+if (!process.env.DATABASE_URL){
+    throw new Error('DATABASE_URL env variable is not set')
+}
+
+export const config: Knex.Config = {
+    client: 'sqlite',
+    connection: {
+        filename: env.DATABASE_URL
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extension: 'ts',
+        directory: './db/migrations'
+
+    }
+}
+
+export const knex = setupKnex(config);
+
+
